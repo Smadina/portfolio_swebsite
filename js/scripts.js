5af3010dae5a13a4541bb499a663200694fa0f1f
Smadina <60246010+Smadina@users.noreply.github.com>
2020-11-11 15:03:39 -0500

    Hamburger Menu Fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 
-function myFunction() {
-  var x = document.getElementById("myNavtoggle");
-  if (x.className === "navtoggle") {
-    x.className += "responsive";
+$('#heading-text').text('How to create a hamburger menu with jQuery');
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
   } else {
-    x.className = "navtoggle";
+    x.className = 'navtoggle';
   }
 }
 
