de3bee615ffd117d6f4c092c260403b6a4e0fbb5
Smadina <60246010+Smadina@users.noreply.github.com>
2020-11-11 11:56:21 -0500

    JavaScript Libraries & Plugins

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,22 @@
 function myFunction() {
   var x = document.getElementById("myNavtoggle");
   if (x.className === "navtoggle") {
-    x.className += " responsive";
+    x.className += "responsive";
   } else {
     x.className = "navtoggle";
   }
 }
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1600, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
